4e7ed8d65323219eb09f3c96f5084a6798ebf379
grace-official <76984418+grace-official@users.noreply.github.com>
2022-10-10 17:23:24 -0700

    e
    e

---
+++ Rube Goldberg/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    [SerializeField] Vector3 offSet;
+
+    void Update()
+    {
+        transform.position = player.position + offSet;
+    }
+}
